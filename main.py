a1bd1c473602c9088fe3d873dec04c789b35a2ae
Leonardo Akira Shimabukuro <leonardoashimabukuro@gmail.com>
2021-06-08 14:57:18 -0300

    Divisão de funções

--- main.py
+++ main.py
@@ -10,6 +10,36 @@ test2 = np.array([[5/math.sqrt(5), 4/math.sqrt(5), 1/math.sqrt(5), 0],
                   [0, 3/math.sqrt(5), 2/math.sqrt(5), 0],
                   [0, 1, 2, 1],
                   [0, 0, 1, 2]])
+
+def qrDecompose(matriz):
+    tamanho = len(matriz)
+    A = matriz
+    givensParameters = []
+    
+    for i in range(tamanho-1):
+        givensParameters.append(getGivensParameters(A[i][i], A[i+1][i]))
+        A = applyGivensRotation(A, givensParameters[i][0], givensParameters[i][1], i)
+        print(A)
+
+def getGivensParameters(alfa, beta):
+    if abs(alfa) > abs(beta):
+        tau = -beta/alfa
+        c = 1/math.sqrt(1+tau**2)
+        s = c*tau
+    else:
+        tau = -alfa/beta
+        s = 1/math.sqrt(1+tau**2)
+        c = s*tau
+    return c, s
+
+def applyGivensRotation(matriz, c, s, k):
+    qa = np.identity(len(matriz))
+    qa[k][k] = c
+    qa[k][k+1] = -s
+    qa[k+1][k] = s
+    qa[k+1][k+1] = c
+    return qa.dot(matriz)
+
 def qrstep(matriz, k):
     """Calcula a matriz da rotação de Givens para alterar a coluna k"""
 
@@ -33,4 +63,4 @@ def qrstep(matriz, k):
 
     return q
 
-print(qrstep(test, 0).dot(test))
+qrDecompose(test)
