53c1461e11cea32f4e1f2649f19fd944b644d15d
Leonardo Akira Shimabukuro <leonardoashimabukuro@gmail.com>
2021-06-11 17:53:14 -0300

    Consolidação e renomeação de funções, documentação

---
+++ README.md
@@ -0,0 +1,26 @@
+# EP1 de Numérico 2021
+
+Este repositório contém o código em Python do algorítmo QR com deslocamento
+espectral para o cálculo dos auto-valores e auto-vetores de uma matriz
+tridiagonal simétrica n × n.
+
+Nesta implementação, foi priorizada a simplicidade do código em detrimento da
+eficiência e velocidade de execução.
+
+# Implementação
+
+## Decomposição QR
+### **getGivensMatrix(matriz, linha)**
+Esta função recebe uma matriz quadrada e o número do elemento da diagonal
+principal (matriz[linha][linha]). A função retorna uma matriz de rotação de
+Givens que zera o valor do elemento abaixo do elemento da diagonal principal
+(matriz[linha][linha+1]).
+
+### **getQRDecomposition(matriz)**
+Esta função recebe uma matriz tridiagonal simétrica e retorna uma tupla
+contendo as matrizes Q e R da decomposição QR.
+
+## Algoritmo QR com deslocamento espectral
+### **getMik(matriz)**
+Calcula o múltiplo da identidade µk que será utilizado no algoritmo QR com
+deslocamento espectral.
--- main.py
+++ main.py
@@ -11,56 +11,51 @@ test2 = np.array([[5/math.sqrt(5), 4/math.sqrt(5), 1/math.sqrt(5), 0],
                   [0, 1, 2, 1],
                   [0, 0, 1, 2]])
 
-def qrDecompose(matriz):
+def getGivensMatrix(matriz, linha):
+    """Esta função recebe uma matriz quadrada e o número do elemento da diagonal
+    principal (matriz[linha][linha]). A função retorna uma matriz de rotação de
+    Givens que zera o valor do elemento abaixo do elemento da diagonal principal
+    (matriz[linha][linha+1])."""
+    alfa = matriz[linha][linha]
+    beta = matriz[linha+1][linha]
+    if abs(alfa) > abs(beta):
+        tau = -beta/alfa
+        c = 1/math.sqrt(1+tau**2)
+        s = c*tau
+    else:
+        tau = -alfa/beta
+        s = 1/math.sqrt(1+tau**2)
+        c = s*tau
+    givensRotationMatrixQ = np.identity(len(matriz))
+    givensRotationMatrixQ[linha][linha] = c
+    givensRotationMatrixQ[linha][linha+1] = -s
+    givensRotationMatrixQ[linha+1][linha] = s
+    givensRotationMatrixQ[linha+1][linha+1] = c
+    return givensRotationMatrixQ
+
+def getQRDecomposition(matriz):
+    '''Esta função recebe uma matriz tridiagonal simétrica e retorna uma tupla
+    contendo as matrizes Q e R da decomposição QR.'''
     tamanho = len(matriz)
-    A = matriz
-    givensParameters = []
-    
+    Qt = np.identity(tamanho) # Esta variável contém a matriz Q transposta
     for i in range(tamanho-1):
-        givensParameters.append(getGivensParameters(A[i][i], A[i+1][i]))
-        A = applyGivensRotation(A, givensParameters[i][0], givensParameters[i][1], i)
-        print(A)
+        Qi = getGivensMatrix(matriz, i)
+        matriz = Qi @ matriz
+        Qt = Qi @ Qt
+    return Qt.transpose(), matriz
 
-def getGivensParameters(alfa, beta):
-    if abs(alfa) > abs(beta):
-        tau = -beta/alfa
-        c = 1/math.sqrt(1+tau**2)
-        s = c*tau
+def getMik(matriz):
+    '''Calcula o múltiplo da identidade µk que será utilizado no algoritmo QR
+    com deslocamento espectral.'''
+    n = len(matriz)
+    dk = (matriz[n-2][n-2] - matriz[n-1][n-1])/2
+    if dk >= 0:
+        mik = matriz[n-1][n-1] + dk - math.sqrt(dk**2 + matriz[n-1][n-2]**2)
     else:
-        tau = -alfa/beta
-        s = 1/math.sqrt(1+tau**2)
-        c = s*tau
-    return c, s
+        mik = matriz[n-1][n-1] + dk + math.sqrt(dk**2 + matriz[n-1][n-2]**2)
+    return mik
 
-def applyGivensRotation(matriz, c, s, k):
-    qa = np.identity(len(matriz))
-    qa[k][k] = c
-    qa[k][k+1] = -s
-    qa[k+1][k] = s
-    qa[k+1][k+1] = c
-    return qa.dot(matriz)
-
-def qrstep(matriz, k):
-    """Calcula a matriz da rotação de Givens para alterar a coluna k"""
-
-    alfa = matriz[k][k]
-    beta = matriz[k+1][k]
-
-    if abs(alfa) > abs(beta):
-        tau = -beta/alfa
-        c = 1/math.sqrt(1+tau**2)
-        s = c*tau
-    else:
-        tau = -alfa/beta
-        s = 1/math.sqrt(1+tau**2)
-        c = s*tau
-    
-    q = np.identity(len(matriz))
-    q[k][k] = c
-    q[k][k+1] = -s
-    q[k+1][k] = s
-    q[k+1][k+1] = c
-
-    return q
-
-qrDecompose(test)
+Q, R = getQRDecomposition(test)
+print(Q)
+print(R)
+print(Q @ R)
